2aacf0feb94712b81dcf96fe957f488a169d3f8e
bilalmakayasa <bilal.makayasa@gmail.com>
2025-02-18 18:59:06 +0700

    fix: footer wording

--- src/components/Home/footer.tsx
+++ src/components/Home/footer.tsx
@@ -1,14 +1,12 @@
 export default function FooterComponent() {
   return (
     <footer className="bg-footer text-center py-20 mt-10 flex flex-col items-center justify-center gap-y-5">
-      <p className="text-main font-title text-4xl">
-        amokerja
-      </p>
+      <p className="text-main font-title text-4xl">amokerja</p>
       <p className="text-black font-helvetica px-72 text-lg">
-        Situs web amokerja.id diurus oleh Perkumpulan Pegiat Kesehatan
-        Masyarakat. Kami berusaha agar selalu menyajikan informasi terbaru
-        tentang kesehatan buruh di Indonesia. Jikalau Anda butuh layanan
-        darurat, hubungi kantor serikat buruh terdekat.
+        Situs web amokerja.id diurus oleh Posko Curhat Buruh. Kami berusaha agar
+        selalu menyajikan informasi terbaru tentang kesehatan buruh di
+        Indonesia. Jikalau Anda butuh layanan darurat, hubungi kantor serikat
+        buruh terdekat
       </p>
     </footer>
   );
